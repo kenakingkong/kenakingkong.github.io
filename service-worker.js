430209e2cab2d29941c3071e2d5f9aff764cc21c
kenakingkong <kena.kong@gmail.com>
2020-09-16 01:04:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cd7fc399defd5defcae3e767ba3b719.js"
+  "/precache-manifest.cb05810bece67cc8b1a918dfd5c14233.js"
 );
 
 workbox.clientsClaim();
